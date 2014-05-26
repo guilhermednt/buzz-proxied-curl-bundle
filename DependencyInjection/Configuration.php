4fda40e055ad5638dad9f760966a6d2acac7149a
Guilherme Donato <guilhermednt@users.noreply.github.com>
2014-05-26 16:50:52 -0300

    Fixed typo

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Donato\HttpServiceBundle\DependencyInjection;
+namespace Donato\BuzzProxiedCurlBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
@@ -18,7 +18,6 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('donato_http_service');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
